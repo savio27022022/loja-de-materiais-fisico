d348527ad38ea18bcc8237bbbf3d0713ba72f455
Sávio <115809910+savio27022022@users.noreply.github.com>
2024-12-11 02:21:49 -0300

    Update testar_function.sql

--- testar_function.sql
+++ testar_function.sql
@@ -1,1 +1,11 @@
+- Utilizar o banco
+USE LojaDeMateriais;
+-- Chamando a função para obter o resultado
+SELECT CONCAT('--------------produtos e categorias ------->', contar_combinacoes()) AS resultado;
 
+-- Consulta final
+SELECT CONCAT('--------------produtos e categorias ------->', total_combinacoes) AS resultado
+FROM (
+    SELECT COUNT(*) AS total_combinacoes
+    FROM produto_categoria
+) AS subquery;
