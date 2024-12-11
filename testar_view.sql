9ec88d8c32d23f9d1e59ba84106f751651d4ccf2
Sávio <115809910+savio27022022@users.noreply.github.com>
2024-12-11 07:37:59 -0300

    Update testar_view.sql

--- testar_view.sql
+++ testar_view.sql
@@ -1,1 +1,5 @@
+SELECT * FROM produtos_com_categorias_fornecedores;
 
+-- Consultando a view
+SELECT CONCAT('--------------produtos e categorias ------->', total_combinacoes) AS resultado
+FROM vw_total_combinacoes;
